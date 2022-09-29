0e97b6b1784b5d43f16fb3b005af53f188c2a629
unknown <alexsrk09@gmail.com>
2022-09-29 10:44:30 +0200

    ej 5

--- unit3/bucles/bucles1.php
+++ unit3/bucles/bucles1.php
@@ -3,6 +3,6 @@
 /** Escribir los números 1 al 10 */
 for ($i = 1; $i <= 10; $i++)
     echo ($i);
-echo ("<br>");
-echo ("<a href=\"https://github.com/alexsrk09/DWES\">github</a>");
+    echo ("<br>");
+    echo ("<a href=\"https://github.com/alexsrk09/DWES/blob/main/unit3/bucles/bucles1.php\">github</a>");
 ?>
--- unit3/bucles/bucles2.php
+++ unit3/bucles/bucles2.php
@@ -6,6 +6,6 @@ for ($i=0; $i <7; $i++) {
     if($i%2==0)$suma=$suma+$i;
 }
 echo($suma);
-echo("<br>");
-echo("<a href=\"https://github.com/alexsrk09/DWES\">github</a>");
+echo ("<br>");
+echo ("<a href=\"https://github.com/alexsrk09/DWES/blob/main/unit3/bucles/bucles2.php\">github</a>");
 ?>
