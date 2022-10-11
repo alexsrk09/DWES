b5a369d9e117db65a7c047e22c06f38c4e1b7f61
unknown <alexsrk09@gmail.com>
2022-10-11 08:43:07 +0200

    bucles 3
    con formulario aleatorio

--- unit3/bucles/bucles3.php
+++ unit3/bucles/bucles3.php
@@ -8,8 +8,10 @@
     <title>Document</title>
 </head>
 <body>
+    <form method="post" action="">
     <table>
         <?php
+        $aciertos=0;
         $desde=1;
         $hasta=10;
             for ($i = $desde; $i <= $hasta; $i++){
@@ -17,13 +19,29 @@
                 if($i!=$desde)echo ("<td style=\" background-color:red;\">".$i."</td>");
                 else echo ("<td style=\" background-color:red;\">"."x"."</td>");
                 for ($j = $desde; $j <= $hasta; $j++){
-                    if($i!=$desde) echo ("<td>".$i*$j."</td>");
+                    if($i!=$desde) {
+                        if((bool)rand(0,1)==true) echo ('<td><input type="number" name="'.$i.'*'.$j.'"/></td>');
+
+                        else echo ("<td>".$i*$j."</td>");
+
+                    }
                     else echo ("<td style=\" background-color:red;\">".$i*$j."</td>");
                 }
                 echo("</tr>");
             }
         ?>
     </table>
+    <input type="submit"/>
+    </form>
+    <?php 
+        if($_POST){
+            foreach($_POST as $name=>$value){
+                $separado = explode("*", $name);
+                if($separado[0]*$separado[1]==$value)$aciertos++;
+            }
+        }
+        echo 'aciertos: '.$aciertos;
+    ?>
 </body>
 </html>
 <?php
