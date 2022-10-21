032b50e036603c2fe9cbeb34976030dde2b61dd3
unknown <alexsrk09@gmail.com>
2022-10-21 13:23:00 +0200

    a

--- proyectos/verbos irregulares/index.php
+++ proyectos/verbos irregulares/index.php
@@ -13,8 +13,8 @@
         <label for="numeroVerbos">Número de verbos&nbsp;</label>
         <input type="number" name="numeroVervos" min="1" value="1">
         <br><br>
-        <label for="numeroCasillas">Número de casillas</label>
-        <input type="number" name="numeroCasillas" min="1" value="1">
+        <label for="numeroCasillas">Número de casillas (minimo 1, máximo 3)</label>
+        <input type="number" name="numeroCasillas" min="1" max="3"value="1">
         <input type="submit" value="Enviar">
     </form>
 </body>
