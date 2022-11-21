0656c9723d3f1c16e636b0d99d3d40d824f5801e
Alex JL <alexsrk09@gmail.com>
2022-11-21 10:18:31 +0100

    conexion a BBDD con PDO

--- proyectos/examen_con_array/configs.php
+++ proyectos/examen_con_array/configs.php
@@ -5,10 +5,12 @@
 $user="user";
 $password="user";
 $abonos = 10;
-
-$conexion = new mysqli("localhost", "baloncesto", "baloncesto", "baloncesto_pokemons");
-if ($conexion->connect_errno) {
-    echo "Fallo al conectar a MySQL: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
+//conexion a la base de datos conPDO
+try {
+    $conexion = new PDO("mysql:host=localhost;dbname=baloncesto_pokemons", "baloncesto", "baloncesto");
+}
+catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
 }
 $tarifas = array(
     array('equipo'=>'Picapiedras', 'tarifas'=>array(
